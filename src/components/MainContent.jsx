be9546fd5d37f7674a8cdea446e450a90899ce3c
Binod Manandhar <binodmanandhar7@gmail.com>
2026-05-27 10:32:31 +1000

    update MainContent.jsx file

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -11,7 +11,7 @@ function MainContent() {
 
   return (
     <main>
-      <button onClick={toggle}>{helpVisible ? "Hide" : "Show"} Help</button>
+      <button onClick={toggleH}>{helpVisible ? "Hide" : "Show"} Help</button>
       {helpVisible && <HelpArea />}
     </main>
   );
